bc5e6cb218fa741e7dd3a1a20b340590d952bf69
Kzmaster <kzanders@mcad.edu>
2016-11-02 10:07:56 -0500

    Initial Committ

---
+++ assignment1/HPlusSport_Start/script.js
@@ -0,0 +1,5 @@
+$(function () {
+	$("#products h2.product-name[data-type='mineralwater']").css("background-color", "blue");
+	$("#products h2.product-name[data-type='vitamin']").css("background-color", "green");
+	$("#products h2.product-name[data-type='proteinbar']").css("background-color", "purple");
+});
